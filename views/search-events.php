7f8f855d3d0ece3756234618a1581f1cc877f3b7
ULTRAPOLLO\pochi <sesegoku@gmail.com>
2025-10-26 20:46:38 +0100

    Fixed the surprise for Tacio

--- views/search-events.php
+++ views/search-events.php
@@ -169,7 +169,7 @@ $tiposEventos = $stmtTipos->fetchAll(PDO::FETCH_COLUMN);
                                     <div class="event-image">
                                         <img src="<?php echo UPLOADS_URL . '/' . $evento['imagenPrincipal']; ?>" 
                                              alt="<?php echo htmlspecialchars($evento['nombre']); ?>"
-                                             onerror="this.src='https://imgur.com/a/tK1HnYn'">
+                                             onerror="this.src='https://i.imgur.com/yGh7PhM.jpeg'">
                                         <span class="event-category"><?php echo htmlspecialchars($evento['tipo']); ?></span>
                                         <button class="favorite-btn">
                                             <i class="far fa-heart"></i>
